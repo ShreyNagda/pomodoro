4102d878fbd712e9848ebda30b3bf4202eeb00cc
Shrey <shreynagda2714@gmail.com>
2023-04-29 13:05:33 +0530

    Notifications delete on close implemented

--- android/app/src/main/kotlin/com/shreynagda/pomodoro/MainActivity.kt
+++ android/app/src/main/kotlin/com/shreynagda/pomodoro/MainActivity.kt
@@ -1,6 +1,25 @@
 package com.shreynagda.pomodoro
 
+//import androidx.annotation.NonNull
+//import io.flutter.embedding.engine.FlutterEngine
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+
+//    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+//        super.configureFlutterEngine(flutterEngine)
+//        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "flutter.temp.channel").setMethodCallHandler {
+//                call, result ->
+//            // This method is invoked on the main thread.
+//            // TODO
+//        }
+//    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("SHREY", "onDestroy");
+        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger!!, "flutter.temp.channel").invokeMethod("destroy", null, null);
+//        Toast.makeText(baseContext, "onDestroy", Toast.LENGTH_SHORT).show()
+    }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,7 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:circle_list/circle_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kplayer/kplayer.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:pomodoro/model/timer_model.dart';
@@ -69,55 +70,28 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   AssetsAudioPlayer assetsAudioPlayer = AssetsAudioPlayer.newPlayer();
 
   @override
-  Future<void> dispose() async {
-    print('dispose');
-    timerController.dispose();
-    // localNotificationsPlugin.cancelAll();
-    // WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  // @override
-  // Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
-  //   // TODO: implement didChangeAppLifecycleState
-  //   super.didChangeAppLifecycleState(state);
-  //   switch (state) {
-  //     case AppLifecycleState.resumed:
-  //       this.state = null;
-  //       print('Resume');
-  //       break;
-  //     case AppLifecycleState.inactive:
-  //       this.state = state;
-  //       print('Inactive');
-  //       break;
-  //     case AppLifecycleState.paused:
-  //       this.state = state;
-  //       print('Paused');
-  //       break;
-  //     case AppLifecycleState.detached:
-  //       print('Detached');
-  //       break;
-  //   }
-  // }
-
-  @override
   void initState() {
     WidgetsBinding.instance.addObserver(this);
-    // width = 100.w;
-    // spacer = 10.w;
     setTimer();
     if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
       player = Player.asset('assets/audio/bell.mp3');
-      player.play();
     }
+    const MethodChannel("flutter.temp.channel").setMethodCallHandler(platformCallHandler);
     super.initState();
   }
 
+  Future<dynamic> platformCallHandler(MethodCall call) async {
+    if (call.method == "destroy"){
+      print("destroy");
+      dispose();
+    }
+  }
+  @mustCallSuper
   @override
-  Future<bool> didPopRoute() async {
+  Future<void> dispose() async {
+    timerController.dispose();
     await service.deleteNotification(0);
-    print('Closed');
-    return super.didPopRoute();
+    super.dispose();
   }
 
   @override
@@ -133,14 +107,10 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
           width = constraints.maxHeight / 4;
           spacer = constraints.maxHeight / 100;
         } else {
-          // print('width:${constraints.maxWidth}');
           width = constraints.maxWidth / 4;
           spacer = constraints.maxWidth / 100;
         }
-        // print('width: $width');
-        // print('spacer: $spacer');
       }
-      // return CustomTimer(timerController: timerController);
       return TimerControllerListener(
           controller: timerController,
           listener: (context, value) async {
@@ -156,10 +126,9 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
             if (value.status == TimerStatus.finished) {
               if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
                 player.play();
-                // print(player.media);
               } else {
-                AssetsAudioPlayer.playAndForget(
-                    Audio('assets/audios/bell.mp3'));
+                await AssetsAudioPlayer.newPlayer().open(Audio('assets/audio/bell.mp3'), autoStart: true,loopMode: LoopMode.none);
+
               }
               changeTimerMode();
               if (prefs.getBool(Constants.autoStartBreakKey)! &&
